106a085d7ee36fdd6a31314773dfa127c679aa0e
VISHESH REDDY <svisheshreddy@gmail.com>
2026-09-23 19:29:34 +0530

    Add backend/routes/comments.js

---
+++ backend/routes/comments.js
@@ -0,0 +1,1 @@
+const r=require('express').Router();const c=require('../controllers/commentController');const {auth}=require('../middleware/auth');r.post('/',auth,c.create);r.delete('/:id',auth,c.remove);module.exports=r;
