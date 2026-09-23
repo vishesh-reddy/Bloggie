b49ffcf816014555653bc07e358e3db2d148a1fc
VISHESH REDDY <svisheshreddy@gmail.com>
2026-09-23 19:29:28 +0530

    Add backend/routes/posts.js

---
+++ backend/routes/posts.js
@@ -0,0 +1,1 @@
+const r=require('express').Router();const c=require('../controllers/postController');const {auth}=require('../middleware/auth');r.get('/',c.list);r.get('/mine',auth,c.mine);r.get('/:slug',c.getBySlug);r.post('/',auth,c.create);r.put('/:id',auth,c.update);r.delete('/:id',auth,c.remove);module.exports=r;
